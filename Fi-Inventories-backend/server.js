338eb7a0b6758d852aff62a58e5354755818628a
MOHAMMED_ZULQARNAIN <zulqarnain4292@gmail.com>
2025-07-30 15:51:45 +0530

    Refactor CORS middleware to simplify origin handling and enhance configuration

--- Fi-Inventories-backend/server.js
+++ Fi-Inventories-backend/server.js
@@ -14,20 +14,17 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 const allowedOrigins = [
-  'https://fi-inventories-management-frontend.vercel.app',
-  'http://localhost:3000',
+  "https://fi-inventories-management-frontend.vercel.app",
+  "http://localhost:3000",
 ];
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
